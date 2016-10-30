22b669e1ef19887b759216a53d044b596232f1df
Sean Harrison <sah@blackearth.us>
2016-10-30 04:31:12 +0000

    improve install script: + add genproclimit=24 to conf + don't getinfo right after starting the server daemon, it's not yet ready for that. + don't try to build zcash before all the apt-get installs, duh.

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ git clone https://github.com/zcash/zcash.git
 cd zcash
 git checkout v1.0.0
 ./zcutil/fetch-params.sh
-./zcutil/build.sh -j$(nproc)
-sudo apt-get install \
+
+sudo apt-get -y install \
       build-essential pkg-config libc6-dev m4 g++-multilib \
       autoconf libtool ncurses-dev unzip git python \
       zlib1g-dev wget bsdmainutils automake
@@ -18,7 +18,6 @@ echo "rpcuser=username" >>~/.zcash/zcash.conf
 echo "rpcpassword=`head -c 32 /dev/urandom | base64`" >>~/.zcash/zcash.conf
 echo 'gen=1' >> ~/.zcash/zcash.conf
 echo 'equihashsolver=tromp' >> ~/.zcash/zcash.conf
+echo "genproclimit=$(nproc)" >> ~/.zcash/zcash.conf
 
 ./src/zcashd --daemon
-
-./src/zcash-cli getinfo
